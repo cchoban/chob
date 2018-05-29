23fb401698f060b71d9b49a16ffc7a6a0ce620ed
Muhammed Kaplan <gokdeniz91@gmail.com>
2018-05-29 15:12:56 +0200

    new arg: --clean

--- core/FileManager.py
+++ core/FileManager.py
@@ -4,26 +4,26 @@ from Logger import Logger
 from subprocess import run, DEVNULL
 from Logger import Logger as log
 from json import dumps
-from core import JsonParser as js
+
+
 class Manager:
-    def fileExists(path=""):
+    def fileExists(self, path=""):
         try:
             if os.path.exists(path):
                 return True
             else:
                 return False
         except FileNotFoundError as e:
-            helpers.errorMessage(e.strerror)
             exit()
 
-    def createFolder(path):
+    def createFolder(self, path):
         try:
             if not os.path.exists(path):
                 os.makedirs(path)
         except Exception as e:
-            helpers.errorMessage(e.strerror)
             exit()
-    def createFile(path, content =""):
+
+    def createFile(self, path, content=""):
         try:
             if not os.path.exists(path):
                 with open(path, "w") as f:
@@ -31,14 +31,12 @@ class Manager:
                     f.close()
         except Exception as e:
             log.new(e).logError()
-            exit()
 
-    def createJsonFile(self, path, withDict = {}):
+    def createJsonFile(self, path, withDict={}):
         try:
             self.createFile(dumps(withDict))
         except FileNotFoundError or PermissionError or Exception as e:
             log.new(e).logError()
-            helpers.errorMessage(e)
 
     def removeDir(self, path):
         if self.fileExists(path):
@@ -46,25 +44,40 @@ class Manager:
                 rmtree(path)
                 os.removedirs(path)
             except FileNotFoundError or WindowsError or PermissionError or Exception as e:
-                helpers.errorMessage(e.strerror, True)
-                exit()
+                log.new(e).logError()
         else:
-            helpers.infoMessage("Path does not exists while trying to remove it: "+path, True)
+            helpers.infoMessage("Path does not exists while trying to remove it: " + path, True)
 
-    def extractZip(zip, dest):
+    def extractZip(self, zip, dest):
         try:
+            helpers.infoMessage("Unzipping " + zip + " to " + dest)
             zf = zipfile.ZipFile(zip, "r")
             zf = zipfile.ZipFile.extractall(dest)
+            helpers.successMessage("Successfully unzipped " + zip + " to " + dest)
         except WindowsError or PermissionError or FileNotFoundError as e:
             Logger.new(e).logError()
-            helpers.errorMessage(e.strerror)
             exit()
 
-    def extract7z(zip, dest):
+    def extract7z(self, zip, dest):
         try:
-            processArgs = helpers.getCobanBinFolder+"7z.exe e "+zip+" -o"+dest+" -y"
+            processArgs = helpers.getCobanBinFolder + "7z.exe e " + zip + " -o" + dest + " -y"
+            helpers.infoMessage("Unzipping " + zip + " to " + dest)
             runProcess = run(processArgs, stdout=DEVNULL, stderr=DEVNULL)
+            helpers.successMessage("Successfully unzipped " + zip + " to " + dest)
         except WindowsError or PermissionError or FileNotFoundError as e:
             Logger.new(e).logError()
-            helpers.errorMessage(e.strerror)
             exit()
+
+    def cleanup(self):
+        packagesPath = helpers.getCobanPath+"\\packages\\"
+        for i in helpers.installedApps()["installedApps"]:
+            path = packagesPath+i
+            if self.fileExists(path):
+                self.removeDir(path)
+                helpers.successMessage("Removed: " + i)
+
+        for i in os.listdir(packagesPath):
+            path = packagesPath+i
+            if not i in helpers.installedApps()["installedApps"]:
+                self.removeDir(path)
+                helpers.successMessage("Removed: "+i)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse, helpers
 from core.cli import cli
-from core import PackageManager, JsonParser
+from core.packageManager import installPackage, removePackage
 
 if not helpers.has_admin():
     helpers.errorMessage("You need admin permissions to be able use this program.")
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser(description="Coban Package manager")
 parser.add_argument("-S", type=str, help="Install a package")
 parser.add_argument("--update", action="store_true", help="Update package repo list to get updated.")
 parser.add_argument("--doctor", action="store_true", help="Fixes common problems.")
+parser.add_argument("--clean", action="store_true", help="Cleans caching of packages.")
 
 # parser.add_argument("-Ss", nargs='+', help="install a package")
 #TODO: add multiple installation of packages
@@ -23,19 +24,21 @@ arg = parser.parse_args()
 
 
 
-if not arg.S == None:
-    packageManager = PackageManager.Manager(arg.S, arg.skipHash, arg.force)
-    packageManager.installPackage()
+if arg.S:
+    installPackage.main(arg.S, arg.skipHash, arg.force).installer()
 
-if not arg.R == None:
-    packageManager = PackageManager.Manager(arg.R, arg.skipHash, arg.force)
-    packageManager.removePackage()
+if arg.R:
+    removePackage.main(arg.R, arg.skipHash, arg.force).uninstaller()
 
 if arg.doctor:
     cli.main().doctor()
 
 if arg.update:
     cli.main().update()
+
+if arg.clean:
+   cli.main().cleanLeftOvers()
+   
 # elif arg.cmd == "install":
 #     packageInstallation(arg.cmd)
 # elif arg.cmd == "remove":
