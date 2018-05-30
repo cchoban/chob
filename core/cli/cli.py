57f4c7f15cf7ed2981fe6c999a666b8d7ec961e5
Muhammed Kaplan <gokdeniz91@gmail.com>
2018-05-30 23:31:20 +0200

    New arg: -Ss
    With this command you can search for packages

--- core/cli/cli.py
+++ core/cli/cli.py
@@ -2,15 +2,18 @@ import repo, helpers
 from core import http, FileManager, JsonParser
 from Logger import Logger as log
 from . import doctor
+import re
+
+
 class main:
     def __init__(self):
         pass
 
-
     def update(self):
-        helpers.infoMessage("Updating repo "+repo.repos()["programList"])
+        helpers.infoMessage("Updating repo if needed " + repo.repos()["programList"])
+        # TODO: if needed check for file size
         try:
-            http.Http.download(http.Http, repo.repos()["programList"], helpers.getCobanPath+"\\programList", "json")
+            http.Http.download(http.Http, repo.repos()["programList"], helpers.getCobanPath + "\\programList", "json")
         except Exception as e:
             log.new(e).logError()
 
@@ -28,5 +31,17 @@ class main:
     def packages(self):
         self.update()
         js = JsonParser.Parser(repo.repos()["localProgramlist"]).fileToJson()
-        for i in js:
+        return js
+
+    def listPackages(self):
+        for i in self.packages():
             print(i)
+
+    def searchInPackages(self, packageName):
+        for i in self.packages():
+
+            regex = re.search(packageName.lower(), i.lower())
+
+            if regex:
+                helpers.successMessage("Found this package(s): ")
+                print(i)
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ if not helpers.has_admin():
     helpers.errorMessage("You need admin permissions to be able use this program.")
     exit()
 
-
 parser = argparse.ArgumentParser(description="Coban Package manager")
 parser.add_argument("-S", type=str, help="Install a package")
+parser.add_argument("-Ss", type=str, help="Search packages")
 parser.add_argument("--update", action="store_true", help="Update package repo list to get updated.")
 parser.add_argument("--doctor", action="store_true", help="Fixes common problems.")
 parser.add_argument("--clean", action="store_true", help="Cleans caching of packages.")
@@ -16,15 +16,13 @@ parser.add_argument("-y", action="store_true", help="Skips agreements")
 parser.add_argument("--packages", action="store_true", help="Lists all available packages.")
 
 # parser.add_argument("-Ss", nargs='+', help="install a package")
-#TODO: add multiple installation of packages
-#TODO: --force doing some weird things??
+# TODO: add multiple installation of packages
+# TODO: --force doing some weird things??
 parser.add_argument("-R", type=str, help="Remove a package")
 parser.add_argument("-Scc", help="Clean's unused files", action="store_true")
 parser.add_argument("-skipHash", help="Skips of checking hash for files", action="store_true")
 parser.add_argument("--force", help="Forces a installation of package", action="store_true")
 arg = parser.parse_args()
-
-
 
 if arg.S:
     installPackage.main(arg.S, arg.skipHash, arg.force, arg.y).installer()
@@ -39,12 +37,10 @@ if arg.update:
     cli.main().update()
 
 if arg.clean:
-   cli.main().cleanLeftOvers()
+    cli.main().cleanLeftOvers()
 
 if arg.packages:
-    cli.main().packages()
-# elif arg.cmd == "install":
-#     packageInstallation(arg.cmd)
-# elif arg.cmd == "remove":
-#     removingPackage(arg.cmd)
+    cli.main().listPackages()
 
+if arg.Ss:
+    cli.main().searchInPackages(arg.Ss)
