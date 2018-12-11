90a58f9178a365f48d2f5599e4b22a78b8c3bf27
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2018-12-12 00:41:53 +0100

    unnecessary functions and add comments for functions

--- src/windows/winregistry.py
+++ src/windows/winregistry.py
@@ -9,14 +9,30 @@ class Registry:
         self.key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
 
     def installedSoftware(self):
+        """
+        Returns all installed softwares including installation for both arch types.
+
+        :return dict: A list of softwares with their uninstall string.
+        """
+
         data = {}
-        data.update(self.CurrInstaller(self.key))
         data.update(self.CurrUninstaller(self.key))
         data.update(self.CurrUninstaller(self.key64))
 
         return data
 
+
+
+
     def CurrUninstaller(self, key):
+        """
+        Looking for software installed on the system.
+
+        :param key: Registry key to looking for softwares.
+        :return dict: A list of softwares with their uninstall string.
+        """
+
+
         properties = {}
         key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key, 0, winreg.KEY_READ)
         for i in range(0, winreg.QueryInfoKey(key)[0]):
@@ -38,34 +54,14 @@ class Registry:
 
         return properties
 
-    def CurrInstaller(self, key):
-        properties = {}
-        key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products"
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key, 0, winreg.KEY_READ)
-
-        for i in range(0, winreg.QueryInfoKey(key)[0]):
-            skey_name = winreg.EnumKey(key, i)
-            skey = winreg.OpenKey(key, skey_name)
-            install_properties = winreg.OpenKey(skey, 'InstallProperties')
-
-            try:
-                packageName = winreg.QueryValueEx(install_properties, 'DisplayName')[0]
-                packageUninstallString = winreg.QueryValueEx(install_properties, 'UninstallString')[0]
-                data = {
-                    packageName: packageUninstallString
-                }
-                properties.update(data)
-
-            except OSError as e:
-                if e.errno == errno.ENOENT:
-                    # DisplayName doesn't exist in this skey
-                    pass
-            finally:
-                skey.Close()
-
-        return properties
-
     def searchForSoftware(self, packageName):
+        """
+        Searching for a software from dictionary (self.installedSoftware())
+
+        :param packageName: Software/Package name to looking for.
+        :return dict: Returns dict if something is found.
+        """
+
         data = {}
         for prod in self.installedSoftware():
             try:
